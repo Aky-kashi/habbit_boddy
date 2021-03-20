86966f03e55e1809280b3c1e245c055dd25d8f19
kayanoki-akinori <akiakikayakaya7@gmail.com>
2021-03-20 17:01:14 +0900

    seventh commit

--- lib/view/post/page/post_prepararion.dart
+++ lib/view/post/page/post_prepararion.dart
@@ -8,83 +8,43 @@ class PostPreparation extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<PostPreparation> {
-  List<Widget> list = <Widget>[
-    Container(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 70.0),
-        child: TextField(
-          decoration: InputDecoration(
-            labelText: "取り組みたいこと",
-            hintText: "例:筋トレ",
-            hintStyle: TextStyle(color: Colors.grey[300]),
-          ),
-        ),
-      ),
-      padding: const EdgeInsets.all(10.0),
-      alignment: Alignment.bottomCenter,
-    ),
-  ];
-
-  List<Widget> _items = <Widget>[];
-
-  @override
-  void initState() {
-    super.initState();
-    _items = list;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: Center(child: new Text('HabbtBoddy')),
-        backgroundColor: Colors.amber,
-      ),
-      body: SingleChildScrollView(
-        child: new Column(children: <Widget>[
-          RaisedButton(
-              onPressed: buttonPressed,
-              child: Padding(
-                padding: EdgeInsets.only(top: 1.0),
-                child: Icon(
-                  Icons.note_add,
-                  size: 30.0,
+    return Scaffold(
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 70.0),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "タスク",
+                    hintText: "例:筋トレ",
+                    hintStyle: TextStyle(color: Colors.grey[300]),
+                  ),
                 ),
-              )),
-          ListView.builder(
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            itemBuilder: (BuildContext context, int index) {
-              return _items[index];
-            },
-            itemCount: _items.length,
+              ),
+              ButtonTheme(
+                minWidth: 5.0,
+                height: 5.0,
+                child: Expanded(
+                  flex: 1,
+                  child: RaisedButton(
+                    child: const Text('登録'),
+                    color: Colors.white,
+                    shape: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ],
           ),
-        ]),
+        ),
       ),
     );
   }
-
-  void buttonPressed() {
-    int _counter = 2;
-      list.add(
-        Container(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 70.0),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: "取り組みたいこと",
-                hintText: "例:筋トレ",
-                hintStyle: TextStyle(color: Colors.grey[300]),
-              ),
-            ),
-          ),
-          padding: const EdgeInsets.all(10.0),
-          alignment: Alignment.bottomCenter,
-        ),
-      );
-
-      setState(() {
-        _items = list;
-      });
-    }
-  }
+}
